9d0a8808fb5527426946191859d1351a301b7f61
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-07-14 15:46:29 +0500

    Added global exception handler for API exceptions

---
+++ src/main/java/com/letsreadhere/recipeapi/exceptions/ExceptionControllerGlobally.java
@@ -0,0 +1,15 @@
+package com.letsreadhere.recipeapi.exceptions;
+
+import com.letsreadhere.recipeapi.model.DTOs.API_ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerGlobally {@ExceptionHandler(APIException.class)
+public ResponseEntity<API_ResponseDTO> API_ExceptionMethod(APIException e) {
+    API_ResponseDTO responses = new API_ResponseDTO(e.getMessage(), false);
+    return new ResponseEntity<>(responses, HttpStatus.BAD_REQUEST);
+}
+}
