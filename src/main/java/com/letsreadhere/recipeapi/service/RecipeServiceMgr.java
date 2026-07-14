ac82bc67e89c86d0338bb17b162b141ac94459e8
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-07-14 14:33:21 +0500

    Implemented RecipeServiceMgr with addRecipe method and necessary dependencies

--- src/main/java/com/letsreadhere/recipeapi/service/RecipeServiceMgr.java
+++ src/main/java/com/letsreadhere/recipeapi/service/RecipeServiceMgr.java
@@ -1,10 +1,23 @@
 package com.letsreadhere.recipeapi.service;
 
 import com.letsreadhere.recipeapi.model.DTOs.RecipeDTO;
+import com.letsreadhere.recipeapi.model.Recipe;
+import com.letsreadhere.recipeapi.repository.RecipeRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
-public class RecipeServiceMgr implements RecipeService{
+@Service
+@RequiredArgsConstructor
+public class RecipeServiceMgr implements RecipeService {
+
+    private final ModelMapper modelMapper;
+    private final RecipeRepository recipeRepository;
+
     @Override
     public RecipeDTO addRecipe(RecipeDTO recipeDTO) {
-        return null;
+        Recipe recipe = modelMapper.map(recipeDTO, Recipe.class);
+        recipe = recipeRepository.save(recipe);
+        return modelMapper.map(recipe, RecipeDTO.class);
     }
 }
