baeb4b1ee3f22bffcc230ab58e784d9c8602877b
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-07-14 12:55:57 +0500

    Add validation constraints to Recipe model fields

--- src/main/java/com/letsreadhere/recipeapi/model/Recipe.java
+++ src/main/java/com/letsreadhere/recipeapi/model/Recipe.java
@@ -1,21 +1,29 @@
 package com.letsreadhere.recipeapi.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Recipe {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, name = "Category cant be null")
+    @NotBlank(message = "Don't leave it empty")
+    @Column(nullable = false, name = "Category")
     private String category;
+
+    @NotBlank(message = "Don't leave it empty")
+    @Column(nullable = false, name = "Name")
+    private String name;
 
     @Size(min = 0, max = 5, message = "The rating must not outlimit between {min} and {max}")
     private Integer rating;
