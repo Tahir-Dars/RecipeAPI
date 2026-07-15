293a7adc1ef50de5a6781f1f834b2cf9e49cdbd1
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-07-15 11:13:44 +0500

    Added findAllByCategory method to RecipeRepository for retrieving recipes by category

--- src/main/java/com/letsreadhere/recipeapi/repository/RecipeRepository.java
+++ src/main/java/com/letsreadhere/recipeapi/repository/RecipeRepository.java
@@ -2,8 +2,13 @@ package com.letsreadhere.recipeapi.repository;
 
 import com.letsreadhere.recipeapi.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    @Query("SELECT e FROM Recipe e WHERE LOWER(e.category) = ?1")
+    List<Recipe> findAllByCategory(String category);
 }
